a6b09e4f00ee8ebbff1a9fd0c405d243ddbff781
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-04-15 23:06:31 -0300

    feat: Implementando as interfaces

--- CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace CleanArchMvc.Domain.Interfaces
 {
     public interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
     }
 }
