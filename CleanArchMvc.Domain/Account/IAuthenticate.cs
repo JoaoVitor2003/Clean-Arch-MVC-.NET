bf55cfa2da60b8d267667c2852589ea4a500e191
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-05-22 23:38:16 -0300

    feat: Incluindo as novas interfaces de usuário.

---
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticate(string username, string password);
+        Task<bool> RegisterUser(string username, string password);
+        Task Logout();
+    }
+}
