ab4144a3525b91977fd593a9328e24405a4957eb
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-05-23 21:03:14 -0300

    feat: Implementando o authenticate.

--- CleanArchMvc.Domain/Account/IAuthenticate.cs
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -8,8 +8,8 @@ namespace CleanArchMvc.Domain.Account
 {
     public interface IAuthenticate
     {
-        Task<bool> Authenticate(string username, string password);
-        Task<bool> RegisterUser(string username, string password);
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> RegisterUser(string email, string password);
         Task Logout();
     }
 }
