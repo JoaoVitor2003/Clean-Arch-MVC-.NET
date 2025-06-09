afe1704852ca9e5f37d8f34995ba56b3ca82731d
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-06-09 20:44:56 -0300

    feat: Implementando o login no Account Controller.

--- CleanArchMvc.WebUI/Controllers/AccountController.cs
+++ CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -1,14 +1,45 @@
 ﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class AccountController : Controller
     {
         private readonly IAuthenticate _authentication;
-        public AccountController(IAuthenticate authentication) 
+        public AccountController(IAuthenticate authentication)
         {
             _authentication = authentication;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
         }
     }
 }
