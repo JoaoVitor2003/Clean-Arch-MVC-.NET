40afdd9c8b0be71a8c9761d4196c50d852e835f7
João Vitor <joaovitor.oliveira1912@gmail.com>
2025-08-12 23:15:46 -0300

    feat: Adicionando o método delete

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -61,5 +61,20 @@ namespace CleanArchMvc.API.Controllers
             
             return Ok(categoryDto);
         }
+
+        [HttpDelete]
+        public async Task<ActionResult<CategoryDTO>> Delete(int? id)
+        {
+            var category = await _categoryService.GetById(id);
+            
+            if (category == null)
+            {
+                return NotFound($"Category with id {id} not found.");
+            }
+
+            await _categoryService.Remove(id);
+
+            return Ok(category);
+        }
     }
 }
